94868b7f24f9d446f13be17f1a60d2e1a26285b7
Andrew Fugate <andrewfugate1121@yahoo.com>
2023-07-10 16:14:35 -0400

    FileInputBeingRead
    Provided file input is being read entirely and accurately

--- src/PA2/PA2.java
+++ src/PA2/PA2.java
@@ -4,11 +4,31 @@
 
 package PA2;
 
+import java.util.*;
+import java.io.*;
+
 public class PA2
 {
 	public static void main(String[] args)
 	{
-		//gg
+		File FileIn = new File("cop3503-asn2-input.txt");
+		Scanner File = null;
+		
+		try
+		{
+			File = new Scanner ( FileIn );
+		}
+		
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		
+		do
+		{
+			System.out.println( File.nextLine() );
+		} while( File.hasNextLine() );
+		
 	}
 }
 
